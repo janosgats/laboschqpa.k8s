bbbbaddfcd85d71274d2f8232d12638e7b34c7d7
Dzsani <gatsjanos@gmail.com>
2021-05-23 02:22:06 +0200

    Using prod TLS certificates

--- gke/setup/setup.sh
+++ gke/setup/setup.sh
@@ -1,13 +1,16 @@
+helm install my-release ingress-nginx/ingress-nginx
+
 kubectl -n=qpa create secret generic protected-ingress-basic-auth-secret --from-file=auth=protected-ingress-basic-auth.htpasswd
 
 kubectl -n=qpa create secret generic laboschqpa-server-secrets --from-file=secrets.properties=server-secrets.properties
 kubectl -n=qpa create secret generic laboschqpa-filehost-secrets --from-file=secrets.properties=filehost-secrets.properties
+kubectl -n=qpa create secret generic laboschqpa-imageconverter-secrets --from-file=secrets.properties=imageconverter-secrets.properties
 
 
 gcloud iam service-accounts add-iam-policy-binding --role roles/iam.workloadIdentityUser --member "serviceAccount:ringed-bebop-312422.svc.id.goog[qpa/cloud-sql-auth-proxy]" cloud-sql-auth-proxy@ringed-bebop-312422.iam.gserviceaccount.com
 
 
-kubectl create namespace cert-manager
 helm repo add jetstack https://charts.jetstack.io
 helm repo update
-helm install cert-manager jetstack/cert-manager --namespace cert-manager --version v1.2.0 --create-namespace
+helm install cert-manager jetstack/cert-manager --namespace cert-manager --version v1.2.0
+kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.2.0/cert-manager.crds.yaml

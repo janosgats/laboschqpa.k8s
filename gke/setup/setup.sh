906c00ac7a35780889f7759994d7dbd4d300c938
Dzsani <gatsjanos@gmail.com>
2021-09-21 04:17:56 +0200

    Switch gcp project to qpa-web-3

--- gke/setup/setup.sh
+++ gke/setup/setup.sh
@@ -1,3 +1,5 @@
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm repo update
 helm install main-release ingress-nginx/ingress-nginx
 
 kubectl -n=qpa create secret generic protected-ingress-basic-auth-secret --from-file=auth=protected-ingress-basic-auth.htpasswd
@@ -7,7 +9,7 @@ kubectl -n=qpa create secret generic laboschqpa-filehost-secrets --from-file=sec
 kubectl -n=qpa create secret generic laboschqpa-imageconverter-secrets --from-file=secrets.properties=imageconverter-secrets.properties
 
 
-gcloud iam service-accounts add-iam-policy-binding --role roles/iam.workloadIdentityUser --member "serviceAccount:qpa-web-2.svc.id.goog[qpa/cloud-sql-auth-proxy]" cloud-sql-auth-proxy@qpa-web-2.iam.gserviceaccount.com
+gcloud iam service-accounts add-iam-policy-binding --role roles/iam.workloadIdentityUser --member "serviceAccount:qpa-web-3.svc.id.goog[qpa/cloud-sql-auth-proxy]" cloud-sql-auth-proxy@qpa-web-3.iam.gserviceaccount.com
 
 
 helm repo add jetstack https://charts.jetstack.io
